d36bf1518d7d0b55060f08c2bffdf07d63b95e1d
Ubuntu <ubuntu@ip-172-31-22-198.us-east-2.compute.internal>
2020-03-03 01:01:01 +0000

    inital commit

--- code_challenges.rb
+++ code_challenges.rb
@@ -1,19 +1,67 @@
 # ASSESSMENT 4: Ruby Coding Practical Questions
 
 # --------------------1) Create a method that takes in an array and returns an array with only odd numbers sorted from least to greatest.
-
-
-
-
-
 fullArr1 = [4, 9, 0, '7', 8, true, 'hey', 7, 199, -9, false, 'hola']
 # Expected output: [-9, 7, 9, 199]
 fullArr2 = ['hello', 7, 23, -823, false, 78, nil, '67', 6, 'Number']
 # Expected output: [-823, 7, 23]
+def odd_num(arr)
+    sorted_arr = []
+    arr.filter do |value|
+        if value.is_a? Integer
+        sorted_arr << value 
+        end
+    end
+        arr = []
+        sorted_arr.filter do |value|
+            if value % 2 == 1
+            arr << value 
+            end
+        end
+    arr.sort
+end
+print odd_num(fullArr1)
+puts ""
+print odd_num(fullArr2)
+puts ""
+
 
 
 # --------------------2) Create a class called Bike that is initialized with a model, wheels, and a frame size. The default number of wheels is 2. Create a get_info method that returns a sentence with all the data from the bike object.
+class Bike 
+    def initialize
+        @model = "Trek"
+        @wheels = 2
+        @frame_size = "168cm"
+        @speed = 0
+    end
+    
+    def get_info
+        puts "The #@model bike has #@wheels wheels and a #@frame_size frame. "
+    end
+    
+    def bell
+        puts "Beep beep!"
+    end
+    
+    def speedometer
+        @speed 
+    end
+    
+    def pedal_faster(num)
+        @speed += num
+    end
+    
+    def brake(num)
+        @speed -= num
+        if @speed <= 0
+            @speed = 0
+        end
+    end
+end
 
+new_bike = Bike.new
+new_bike.get_info
 
 
 
@@ -22,7 +70,7 @@ fullArr2 = ['hello', 7, 23, -823, false, 78, nil, '67', 6, 'Number']
 
 
 # -------------------3) Add a bell to the bike class and create a method that will ring the bell when the method is called.
-
+new_bike.bell
 
 
 
@@ -32,7 +80,7 @@ fullArr2 = ['hello', 7, 23, -823, false, 78, nil, '67', 6, 'Number']
 
 # -------------------4) Add a speedometer to the Bike class. The speed should be initialized at 0.
 
-
+puts new_bike.speedometer
 
 
 
@@ -41,7 +89,13 @@ fullArr2 = ['hello', 7, 23, -823, false, 78, nil, '67', 6, 'Number']
 
 # -------------------5) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed. The brake method should decrease the speed. The bike cannot go negative speeds.
 
-
+puts new_bike.speedometer
+new_bike.pedal_faster 10
+puts new_bike.speedometer
+new_bike.pedal_faster 15
+puts new_bike.speedometer
+new_bike.brake 25
+puts new_bike.speedometer
 
 
 
--- interview-questions.md
+++ interview-questions.md
@@ -4,38 +4,38 @@ Answer the following questions. First, without external resources. Challenge you
 
 1. What is an instance variable in Ruby? How is it different from other variables?
 
-  Your answer:
+  Your answer: An instance variable can be accessed by any method of a class, whereas other variables can only be accessed by one method.
 
-  Researched answer:
+  Researched answer: A local variable that is defined inside one method, for example, cannot be accessed by another method. In order to get around this limitation, we can use instance variables inside our Ruby classes. An instance variable is a variable that is accessible in any instance method in a particular instance of a class.
 
 
 
 2. What is a block in Ruby?
 
-  Your answer:
+  Your answer: A way to group elements together.
 
-  Researched answer:
+  Researched answer: A Ruby block is a way of grouping statements, and may appear only in the source adjacent to a method call; the block is written starting on the same line as the method call's last parameter (or the closing parenthesis of the parameter list). The code in the block is not executed at the time it is encountered.
 
 
 
 3. Ruby has an implicit return. What does that mean?
 
-  Your answer:
+  Your answer: Not sure :(
 
-  Researched answer:
+  Researched answer: Any statement in Ruby returns the value of the last evaluated expression. A simple example is the getter method. ... But as we know, Ruby always returns the last evaluated expression.
 
 
 
 4. What is object-oriented programming? How is it different than functional programming?
 
-  Your answer:
+  Your answer: object-oriented programming allows you to store data in objects whereas functional programming does not allow you to do this.
 
-  Researched answer:
+  Researched answer: Functional Programming and Object-oriented programming both are different concepts of programming language. ... In functional programming, data cannot be stored in objects and it can only be transformed by creating functions. In object-oriented programming, data is stored in objects.
 
 
 
 5. What is the difference between a class and an object?
 
-  Your answer:
+  Your answer: A class can hold an object within it. An object is a state that stores values.
 
-  Researched answer:
+  Researched answer: The difference is simple and conceptual. A class is a template for objects. ... An object is a member or an "instance" of a class. An object has a state in which all of its properties have values that you either explicitly define or that are defined by default settings.
